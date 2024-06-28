52921669695bb692054d0dea1220c85e45ba5084
koyyanaadarsh <adarskoyyana@gmail.com>
2024-06-28 13:40:15 +0530

    Update index.js

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,6 +4,7 @@ import TwitterIcon from '@mui/icons-material/Twitter';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import { Bio } from '../../data/constants';
+// import { Bio } from '../../data/constants';
 
 const FooterContainer = styled.div`
   width: 100%;
